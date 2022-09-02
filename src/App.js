cebdad2e2ee2f3ff733f5fd521a81a782c51c068
Roberto Padilha <robertocesardasilva@gmail.com>
2022-09-01 21:57:25 -0300

    Aula 01/09

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const STORAGE_KEY = 'shopping-list'
+
+const App = () => {
+
+    const [amount, setAmount] = useState('')
+    const [description, setDescription] = useState('')
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        const tempList = localStorage.getItem(STORAGE_KEY)
+        
+        if (tempList != null) {
+            setItems(JSON.parse(tempList))
+        }
+    }, [])
+
+    const addItem = () => {
+        const item = {
+            amount, description
+        }
+
+        const newItems = [...items, item]
+        
+        setItems(newItems)
+        setAmount('')
+        setDescription('')
+
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(newItems))
+    }
+
+    return (
+        <div>
+            <h1>Shopping List</h1>
+
+            Adicione um item: <br />
+
+            <input type="number" value={amount}
+                onChange={event => setAmount(event.target.value)} />
+            <input type="text" size="40" value={description}
+                onChange={event => setDescription(event.target.value)} />
+
+            <input type="button" value="+" onClick={addItem}/>
+
+            <ul>
+                {
+                    items.map( (item, index) => (
+                        <li key={index}>{item.amount} - {item.description}</li>
+                    ))
+                }
+            </ul>
+        </div>
+    );
 }
 
 export default App;
